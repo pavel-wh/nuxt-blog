9e274e3952c9f44ca8247eebb9a63854b7071631
webforeman <webforeman@deb>
2019-10-20 07:10:13 +0100

    Add admin layout and admin base page

---
+++ layouts/admin.vue
@@ -0,0 +1,22 @@
+<template>
+    <el-container class="admin">
+        <el-aside width="200px">
+            Aside
+        </el-aside>
+        <el-main>
+            <nuxt />
+        </el-main>
+    </el-container>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="sass">
+    .admin
+        width: 100%
+        height: 100vh
+</style>
---
+++ pages/admin/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Welcome Admin Page</h1>
+</template>
+
+<script>
+export default {
+    layout: 'admin'
+}
+</script>
