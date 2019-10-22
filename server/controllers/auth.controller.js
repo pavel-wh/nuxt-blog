8eebd17efc880d7340988ad1643d04b5e8a45982
webforeman <webforeman@deb>
2019-10-22 07:54:50 +0100

    Add create user to db

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -23,6 +23,22 @@ module.exports.login = async (req, res) => {
     }
 }
 
-module.exports.create = (req, res) => {
-    
+module.exports.create = async (req, res) => {
+    const candidate = await User.findOne({ login: req.body.login })
+    if (candidate) {
+
+        res.status(409).json({ message: 'Логин уже занят' })
+
+    } else {
+        const salt = bcrypt.genSaltSync(10)
+
+        const user = new User({
+            login: req.body.login,
+            password: bcrypt.hashSync(req.body.password, salt)
+        })
+
+        await user.save()
+        res.status(201).json(user)
+
+    }
 }
