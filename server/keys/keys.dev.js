32e93b18742c9d3f170734bf1c9defdcb2b8d721
webforeman <webforeman@deb>
2019-10-22 06:22:53 +0100

    Connect to database in DaaS cloud.mongodb.com

--- server/keys/keys.dev.js
+++ server/keys/keys.dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-    
+    MONGO_URI: 'mongodb+srv://webforeman:882133BPa!@nuxt-blog-bz3fc.mongodb.net/test?retryWrites=true&w=majority'
 }
--- server/keys/keys.prod.js
+++ server/keys/keys.prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-    
+    MONGO_URI: 'mongodb+srv://<username>:<password>@nuxt-blog-bz3fc.mongodb.net/test?retryWrites=true&w=majority'
 }
