5a892071fb2de96f4f971531b06358f7b1988e41
webforeman <webforeman@deb>
2019-10-22 06:37:33 +0100

    Add routes, controllers, models

--- server/app.js
+++ server/app.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const authRoutes = require('./routes/auth.routes')
 const keys = require('./keys')
 const app = express()
 
-mongoose.connect(keys.MONGO_URI)
-    .then(() => console.log('MongoDB connected...'))
-    .catch(error => console.log(error))
+mongoose.connect(keys.MONGO_URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    })
+        .then(() => console.log('MongoDB connected...'))
+        .catch(error => console.log(error))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+app.use('/api/auth', authRoutes)
 
 module.exports = app
